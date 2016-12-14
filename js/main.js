f1c70350f124df24b071dd2a1d7a7c367a1ad293
Klaudia Przespolewska <klaudia1000@onet.eu>
2016-12-14 22:23:30 +0100

    poprawione zadanie

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
-$("button").click(function() {
+$(function() {
+    
+    $("div").css({"height":"200px", "width": "200px", "background-color": "red"}); /*nadanie właściwości kwadratowi*/
+    
+    $("button").click(function() {
+        $("div").animate({"height": "100px", "width": "100px", "margin-left": "100px"}, 3000, hideSquere); /*Funkcja click wywołuje animacje(zmiana wielkości i przesuniecie kwadratu)*/
+        
+     function hideSquere() {
+         $("div").animate({"margin-left": "0px"}).fadeOut(2500, changeColor); /*funkcja zmiany koloru, ktora dzieje się w funkcji click*/
+     };
+        function changeColor() {
+            $("div").css({"background-color": "blue"}).fadeIn(5000,napis);/*funkcja powoduje zmiane koloru*/
+            
+            function napis() {
+                $("div").add("h2").text("Gratulacje, animacja zakończona").css({"color": "black", "font-size" : "20px"});
+            }
+        }
+        
+    });
+    
+});
+                    
+
+
+
+
+
+
+
+
+
+/*$("button").click(function() {
         var square = $(".square");
     
         square.animate({"height" : "100px", "width" : "100px", "margin-left": "100px"},3000);
@@ -10,7 +41,7 @@ function onComplete () {
     $(".square").css("background-color", "blue");
     $(".napis").css("display", "inline");
     
-}
+}*/
     
 
     
