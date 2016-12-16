7ca287fe167b5696a26fd5f934820a11fa5239e1
Klaudia Przespolewska <klaudia1000@onet.eu>
2016-12-16 22:33:44 +0100

    zmiany

--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
+
 $(function() {
     
     $("div").css({"height":"200px", "width": "200px", "background-color": "red"}); /*nadanie właściwości kwadratowi*/
     
     $("button").click(function() {
-        $("div").animate({"height": "100px", "width": "100px", "margin-left": "100px"}, 3000, hideSquere); /*Funkcja click wywołuje animacje(zmiana wielkości i przesuniecie kwadratu)*/
+        $("div").animate({"height": "100px", "width": "100px", "margin-left": "100px"}, 3000, moveSquere); /*Funkcja click wywołuje animacje(zmiana wielkości i przesuniecie kwadratu)*/
+       
         
-     function hideSquere() {
-         $("div").animate({"margin-left": "0px"}).fadeOut(2000, changeColor); /*funkcja zmiany koloru, ktora dzieje się w funkcji click*/
-     };
-        function changeColor() {
-            $("div").css({"background-color": "blue"}).fadeIn(5000,napis);/*funkcja powoduje zmiane koloru*/
+     function moveSquere() {
+         $("div").animate({"margin-left": "0px"},2000, changeColor); /*funkcja powrotu kwadratu na miejsce, ktora dzieje się w funkcji click*/
+     
+       function changeColor() {
+            $("div").css({"background-color": "blue"},5000,napis)};/*funkcja powoduje zmiane koloru, napis się nie wyświetla*/
+         
+         /*$("div").animate({"background-color": "blue"},5000,napis)}; /*napis się wyświetla, kolor się nei zmienia*/
             
             function napis() {
                 $("div").add("h2").text("Gratulacje, animacja zakończona").css({"color": "black", "font-size" : "20px"});
+            /*funkcja powoduje pojawienie się napisu wewnątrz kwadratu*/
             }
         }
         
     });
-    
 });
+
+
+
+
                     
 
 
@@ -29,19 +37,6 @@ $(function() {
 
 
 
-/*$("button").click(function() {
-        var square = $(".square");
-    
-        square.animate({"height" : "100px", "width" : "100px", "margin-left": "100px"},3000);
-        square.animate({"margin-left": "0px"}, 5000, onComplete);
-    
-});
-
-function onComplete () {
-    $(".square").css("background-color", "blue");
-    $(".napis").css("display", "inline");
-    
-}*/
     
 
     
